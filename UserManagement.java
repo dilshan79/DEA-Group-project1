262783baa1b89292cfe508bd27196c8b47d7136b
Avi <94886185+Avish123@users.noreply.github.com>
2022-10-05 23:18:43 +0530

    user management

--- UserManagement.java
+++ UserManagement.java
@@ -9,3 +9,25 @@ import java.util.List;
 
 import pvt.modules.StaffMember;
 import pvt.modules.User;
+public class UserManagement {
+	DbConnection db=new DbConnection();
+	private Connection con =db.getConnection();
+	
+	public int addNewStaffMember(StaffMember stm) {
+		int result=0;
+		try {
+			String query="insert into user(name,email,password,dob,address,role,status) values(?,?,?,?,?,?,?)";
+			PreparedStatement st=con.prepareStatement(query);
+			
+			st.setString(1, stm.getName());
+			st.setString(2, stm.getEmail());
+			st.setString(3, stm.getPassword());
+			st.setString(4, stm.getDob());
+			st.setString(5, stm.getAddress());
+			st.setString(6, stm.getRole());
+			st.setString(7, stm.getAcc_status());
+			
+			result=st.executeUpdate();
+			
+			
+		}
