2ee2c26e0fa0d8b0193026b73f82adfe47d76032
Avi <94886185+Avish123@users.noreply.github.com>
2022-10-05 23:04:31 +0530

    users management

---
+++ UserManagement.java
@@ -0,0 +1,11 @@
+package pvt.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pvt.modules.StaffMember;
+import pvt.modules.User;
