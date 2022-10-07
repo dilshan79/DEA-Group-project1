aa3f7fbd5f2281a291e037a7b2be0c814cac29bd
ushan21729 <95603212+ushan21729@users.noreply.github.com>
2022-10-07 12:35:30 +0530

    update add new staff

--- AddNewStaff.java
+++ AddNewStaff.java
@@ -28,3 +28,33 @@ public class AddNewStaff extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserManagement um=new UserManagement();
+		int result=0;
+		if(request.getParameter("register") != null) {
+			String name=request.getParameter("name");
+			String email=request.getParameter("email");
+			String password=request.getParameter("password");
+			String address=request.getParameter("address");
+			String role=request.getParameter("role");
+			String dob=request.getParameter("dob");
+			
+			if(role.equals("1")) {
+				StaffMember stm=new Level_One(name,dob,email,address,password,"Level 1");
+				result=um.addNewStaffMember(stm);
+			}
+			else if(role.equals("2")) {
+				StaffMember stm=new Level_Two(name,dob,email,address,password,"Level 2");
+				result=um.addNewStaffMember(stm);
+			}
+			
+			if(result>0) {
+				response.sendRedirect("admin_dashboard.jsp");
+			}
+			else {
+				response.sendRedirect("add_new_Staff_member.jsp");
+			}
+		}
+	}
+
+}
