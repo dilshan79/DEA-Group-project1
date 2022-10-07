c30246c2389ca2164291a8aaa48281c5fa94fa7c
Nethmini Paranagamage <97327943+Nethmini98@users.noreply.github.com>
2022-10-07 12:18:20 +0530

    Add files via upload
    I upload one document

---
+++ Flight.java
@@ -0,0 +1,13 @@
+package pvt.modules;
+
+public class Flight {
+	private int id;
+	private String flightNumber;
+	private String departTime;
+	
+	public void setTicketPrice(double ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+	
