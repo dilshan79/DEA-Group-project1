1e00746cac2079477183446a317537796897b4f2
dilshan79 <97287097+dilshan79@users.noreply.github.com>
2022-10-07 10:22:22 +0530

    level_one.java stat

---
+++ Level_One.java
@@ -0,0 +1,27 @@
+package pvt.modules;
+
+import pvt.database.FlightManagement;
+
+public class Level_One extends StaffMember {
+
+	public Level_One() {
+		super();
+		
+	}
+
+	
+		
+	}
+	public void updateTicketDetails() {
+		
+	}
+	public void deleteTicketDetails() {
+		
+	}
+	
+	public void internalChat() {
+		
+	}
+	
+	
+}
