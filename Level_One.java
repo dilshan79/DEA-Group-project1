219fccd29cb4cc8726f53228a88e2930c426c1a3
dilshan79 <97287097+dilshan79@users.noreply.github.com>
2022-10-07 10:23:11 +0530

    complet level_one.java

--- Level_One.java
+++ Level_One.java
@@ -9,7 +9,32 @@ public class Level_One extends StaffMember {
 		
 	}
 
-	
+	public Level_One(String name, String dob, String email, String address, String password, String role) {
+		super(name, dob, email, address, password, role);
+		
+	}
+	public void addFlightDetails() {
+		
+	}
+	public int updateFlightDetails(Flight flight) {
+		int result=0;
+		
+		FlightManagement fm=new FlightManagement();
+		
+		result=fm.updateFlight(flight);
+		
+		return result;
+		
+	}
+	public int deleteFlightDetails(Flight flight) {
+		int result=0;
+		FlightManagement fm=new FlightManagement();
+		
+		result=fm.deleteFlight(flight);
+		
+		return result;
+	}
+	public void checkTicketDetails() {
 		
 	}
 	public void updateTicketDetails() {
